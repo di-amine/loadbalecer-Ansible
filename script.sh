d5885d4f727f565dce020e55c219ccaff4828c00
ittrainingdev <ittrainingdev@hotmail.fr>
2020-02-29 20:31:29 +0100

    update

---
+++ script.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+echo "make new password mysql:"
+read passwd
+
+echo "Stop Service Mysql"
+
+sudo systemctl stop mysqld
+
+echo "Copy file from /etc/my.cnf"
+
+sudo  sed -i -e '$a\skip-grant-tables' /etc/my.cnf
+
+echo "Start service Mysql"
+
+sudo systemctl start mysqld
+
+echo "Change password root"
+
+mysql -h 127.0.0.1 -u root -e \ "FLUSH PRIVILEGES; ALTER USER 'root'@'localhost' IDENTIFIED BY '$passwd'; exit"
+
+echo "Stop Service Mysql"
+sudo systemctl stop mysqld
+
+
+sudo sed -i '$d' /etc/my.cnf
+
+
+echo "Start service Mysql"
+sudo systemctl start mysqld
+systemctl status mysqld
